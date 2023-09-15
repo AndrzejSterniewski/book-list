3d3c95062f0f6306db823b65faf05c40260a554c
Andrzej Sterniewski <drapiszon@gmail.com>
2023-09-15 14:34:30 +0200

    Change code, function calls

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 {
     'use strict';
 
+    /* create reference to .book-list and template*/
+    const bookContainer = document.querySelector('books-list');
+    const templateBook = document.querySelector('#template-book');
+    const favoriteBooks = [];
+
     render();
     initActions();
     addToFavorite();
-
-    /* create reference to .book-list and template*/
-    const bookContainer = document.querySelector('.books-list');
-    const templateBook = document.querySelector('#template-book');
-    const favoriteBooks = [];
 
     /* create function 'render' */
     function render() {
@@ -25,28 +25,28 @@
     }
     /* Add initAcions function */
     function initActions() {
-        const thisBook = this;
+    //    const thisBook = this;
         /* prepare reference to list of all elements book_image in booksList list */
-        const books = bookContainer.querySelectorAll('.book_image');
+        const books = bookContainer.querySelectorAll('.book__image');
         /* walk through every element in the list */
-        for (let book in books) {
+        for (let book of books) {
             /* add EventListener to each item in the list */
             book.addEventListener('dbclick', function (event) {
                 /* prevent default action */
                 event.preventDefault();
                 /* check if book isn't in avorite list */
-                if(!book.getAttribute('class') == '.favorite'){
+                if(!book.classList.contains('class') == 'favorite'){
                     /* add class favorite to every cdbclicked element */
-                book.classList.add('.favorite');
+                book.classList.add('favorite');
                 /* get id from data-id id of the element */
-                const dataId = book.getAttribute('#data-id');
+                const dataId = book.getAttribute('data-id');
                 /* add this id to favoriteBooks array */
                 favoriteBooks.push(dataId);
                 } else {
                     /* remove book from favoriteBooks array */
                     favoriteBooks.indexOf(book).splice(book, 1);
                     /* remove class favorite from book */
-                    book.classList.remove('.favorite');
+                    book.classList.remove('favorite');
                 }
             });
         }
